178d94d1b2367234d6de83ad54e278d1b0f5828b
Jane Kim <jane@janekim.me>
2024-08-06 21:18:50 -0700

    style using tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="grid h-screen">
       <Head>
         <title>@janecakemaster</title>
         <meta name="description" content="@janecakemaster's website" />
-        {/* TODO favicon */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>@janecakemaster</h1>
-        <p className={styles.description}>
-          <code className={styles.code}>
-            fullstack js/golang engineer from the dirty jerz living in az 🌵
-          </code>
-        </p>
+      <main className="place-self-center">
+        <h1 className="text-center">@janecakemaster</h1>
+        <div>
+          <p>
+            <code>
+              fullstack js/golang engineer from the dirty jerz living in az 🌵
+            </code>
+          </p>
+        </div>
       </main>
     </div>
   );
